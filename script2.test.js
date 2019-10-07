542f6f9b1f28c55168b9fcdeb0374fbe7e7bc05c
David <dhahn123@gmail.com>
2019-10-07 14:05:39 -0700

    comments

--- script2.test.js
+++ script2.test.js
@@ -17,5 +17,23 @@ it('calls swapi to get people with a promise', () => {
         expect(data.results.length).toBeGreaterThan(5)
     })
 })
-
 // https://github.com/sapegin/jest-cheat-sheet (USEFUL)
+
+it('getPeople returns count and results', () => {
+// we can use 'mockFetch' as a spy to check on the network requests to 'swapi' API
+    const mockFetch = jest.fn()
+        .mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve({
+                count: 89,
+                results: [0, 1, 2, 3, 4, 5]
+            })
+        }))
+        expect.assertions(4)
+        return swapi.getPeoplePromise(mockFetch).then(data => {
+// 'mock' is included in Jest documentation -> checks calls inside 'getPeoplePromise'
+        expect(mockFetch.mock.calls.length).toBe(1);
+        expect(mockFetch).toBeCalledWith('https://swapi.co/api/people')
+        expect(data.count).toEqual(89)
+        expect(data.results.length).toBeGreaterThan(5)
+    })
+})
